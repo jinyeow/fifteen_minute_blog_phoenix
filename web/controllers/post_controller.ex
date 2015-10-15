f6bac66fbf840b3fa4a6d58c93500d00ebb8bd9b
jinyeow <jin-yeow@outlook.com>
2015-10-15 17:56:08 +1100

    mini blog tutorial finished. go through code later to learn more about Phoenix

--- web/controllers/post_controller.ex
+++ web/controllers/post_controller.ex
@@ -8,7 +8,9 @@ defmodule FifteenMinuteBlogPhoenix.PostController do
   plug :scrub_params, "comment" when action in [:add_comment]
 
   def index(conn, _params) do
-    posts = Repo.all(Post)
+    posts = Post
+    |> Post.count_comments
+    |> Repo.all
     render(conn, "index.html", posts: posts)
   end
 
